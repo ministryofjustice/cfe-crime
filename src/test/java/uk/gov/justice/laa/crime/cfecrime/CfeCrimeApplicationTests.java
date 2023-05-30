6a9bdb766b1ab2a1e5fd544ab1962f987de1215f
Stephen Dicks <github@starswan.com>
2023-05-30 16:52:16 +0100

    LEP-184 Minimum Spring boot app

---
+++ src/test/java/uk/gov/justice/laa/crime/cfecrime/CfeCrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.cfecrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfeCrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
