40e2cb87a94dd7950ea87f914d946d1e5a683e93
Stephen Dicks <github@starswan.com>
2023-06-01 16:30:51 +0100

    Lep 184 project boot (#3)

---
+++ src/main/java/uk/gov/justice/laa/crime/cfecrime/CfeCrimeApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.cfecrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfeCrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfeCrimeApplication.class, args);
+	}
+
+}
