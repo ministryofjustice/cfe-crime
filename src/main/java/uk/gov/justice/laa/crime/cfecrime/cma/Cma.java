e5bc118677483fe98d79c7513761f9e5379267f9
David Read <david.read@digital.justice.gov.uk>
2023-07-04 16:10:49 +0100

    Add whitespace, to see what code-formatter does

--- src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/Cma.java
+++ src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/Cma.java
@@ -4,5 +4,5 @@ import uk.gov.justice.laa.crime.cfecrime.cma.request.CmaRequest;
 import uk.gov.justice.laa.crime.cfecrime.cma.response.ValueList;
 
 public interface Cma {
-    ValueList callCma(CmaRequest request);
+                ValueList callCma(CmaRequest request);
 }
