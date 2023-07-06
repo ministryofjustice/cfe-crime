ade7e486fb9bc22b5ba7ad1bdc3010f1ba711656
Ila <ila.patel@digital.justice.gov.uk>
2023-07-06 11:26:32 +0100

    using lombok

--- src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/enums/MeansTestOutcome.java
+++ src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/enums/MeansTestOutcome.java
@@ -1,18 +1,18 @@
 package uk.gov.justice.laa.crime.cfecrime.cma.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
- * OutCome values for result of assessment
+ * Means Test Outcome values for result of assessment
  */
+@Getter
 public enum MeansTestOutcome {
     ELIGIBLE_WITH_CONTRIBUTION("Eligible with income contribution"),
     ELIGIBLE_WITH_NO_CONTRIBUTION("Eligible with no income contribution"),
     INELIGIBLE("Ineligible");
 
     private String outcome;
-
-    public String getOutcome(){
-        return outcome;
-    }
 
     private MeansTestOutcome(String outcome){
         this.outcome = outcome;
