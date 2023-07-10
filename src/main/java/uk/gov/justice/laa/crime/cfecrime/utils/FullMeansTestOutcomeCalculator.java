9567b2bed3c1b5632940b56c76615696f8241e44
Stephen Dicks <github@starswan.com>
2023-07-10 10:19:01 +0100

    Try to improve code coverage

--- src/main/java/uk/gov/justice/laa/crime/cfecrime/utils/FullMeansTestOutcomeCalculator.java
+++ src/main/java/uk/gov/justice/laa/crime/cfecrime/utils/FullMeansTestOutcomeCalculator.java
@@ -1,17 +1,15 @@
 package uk.gov.justice.laa.crime.cfecrime.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import uk.gov.justice.laa.crime.cfecrime.cma.enums.CaseType;
 import uk.gov.justice.laa.crime.cfecrime.cma.enums.FullAssessmentResult;
 import uk.gov.justice.laa.crime.cfecrime.cma.enums.MagCourtOutcome;
-import uk.gov.justice.laa.crime.cfecrime.cma.enums.CaseType;
 import uk.gov.justice.laa.crime.cfecrime.cma.enums.MeansTestOutcome;
+
 import java.util.Set;
 
 @Slf4j
 public class FullMeansTestOutcomeCalculator {
-
-    private FullMeansTestOutcomeCalculator(){
-    }
 
     public static MeansTestOutcome getFullMeansTestOutcome(FullAssessmentResult result, CaseType caseType, MagCourtOutcome magCourtOutcome){
         log.debug("FullMeansTestOutcome start. Inputs: result = {} caseType = {} magCourtOutcome = {}", result, caseType, magCourtOutcome);
@@ -19,8 +17,7 @@ public class FullMeansTestOutcomeCalculator {
         MeansTestOutcome meansTestOutcome = null;
 
         // Magistrates' court
-        if (caseType == CaseType.SUMMARY_ONLY ||
-            caseType == CaseType.COMMITAL ||
+        if (isMagistrateCourtCaseType(caseType) ||
             (caseType == CaseType.EITHER_WAY && magCourtOutcome != MagCourtOutcome.COMMITTED_FOR_TRIAL && magCourtOutcome != null)) {
                 if (result == FullAssessmentResult.FAIL) {
                     meansTestOutcome = MeansTestOutcome.INELIGIBLE;
@@ -31,28 +28,9 @@ public class FullMeansTestOutcomeCalculator {
         }
 
         // Crown Court
-        if (caseType == CaseType.INDICTABLE ||
-            caseType == CaseType.CC_ALREADY ||
+        if (isCrownCourtCaseType(caseType) ||
             (caseType == CaseType.EITHER_WAY && magCourtOutcome == MagCourtOutcome.COMMITTED_FOR_TRIAL)) {
-                if (result == FullAssessmentResult.INEL) {
-                    meansTestOutcome = MeansTestOutcome.INELIGIBLE;
-                }
-                if (result == FullAssessmentResult.FAIL) {
-                    meansTestOutcome = MeansTestOutcome.ELIGIBLE_WITH_CONTRIBUTION;
-                }
-                if (result == FullAssessmentResult.PASS) {
-                    meansTestOutcome = MeansTestOutcome.ELIGIBLE_WITH_NO_CONTRIBUTION;
-                }
-        }
-
-        // Appeal to Crown Court
-        if (caseType == CaseType.APPEAL_CC) {
-                if (result == FullAssessmentResult.FAIL) {
-                    meansTestOutcome = MeansTestOutcome.ELIGIBLE_WITH_CONTRIBUTION;
-                }
-                if (result == FullAssessmentResult.PASS) {
-                    meansTestOutcome = MeansTestOutcome.ELIGIBLE_WITH_NO_CONTRIBUTION;
-                }
+            meansTestOutcome = crownCourtOutcome(result);
         }
 
         if (meansTestOutcome == null) {
@@ -61,6 +39,36 @@ public class FullMeansTestOutcomeCalculator {
         }
         log.info("FullMeansTestOutcome end. Outputs: meansTestOutcome = {}", meansTestOutcome);
         return meansTestOutcome;
+    }
+
+    private FullMeansTestOutcomeCalculator(){
+    }
+
+    private static Set<CaseType> caseTypesHeardInMagistratesCourt = Set.of(CaseType.COMMITAL, CaseType.SUMMARY_ONLY);
+
+    private static boolean isMagistrateCourtCaseType(CaseType caseType) {
+        return caseType != null && caseTypesHeardInMagistratesCourt.contains(caseType);
+    }
+
+    private static Set<CaseType> caseTypesHeardInCrownCourt = Set.of(CaseType.INDICTABLE,
+            CaseType.CC_ALREADY,
+            CaseType.APPEAL_CC);
+
+    private static boolean isCrownCourtCaseType(CaseType caseType) {
+        return caseType != null && caseTypesHeardInCrownCourt.contains(caseType);
+    }
+
+    private static MeansTestOutcome crownCourtOutcome(FullAssessmentResult result) {
+        switch (result) {
+            case INEL:
+                return MeansTestOutcome.INELIGIBLE;
+            case FAIL:
+                return MeansTestOutcome.ELIGIBLE_WITH_CONTRIBUTION;
+            case PASS:
+                return MeansTestOutcome.ELIGIBLE_WITH_NO_CONTRIBUTION;
+            default:
+                return null;
+        }
     }
 
 }
