a09dce0e673601efdfb64e02686c4633766177a3
Ila <ila.patel@digital.justice.gov.uk>
2023-07-26 17:39:30 +0100

    Removed old cma stub

--- src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/Cma.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.laa.crime.cfecrime.cma;
-
-import uk.gov.justice.laa.crime.cfecrime.api.cma.*;
-import uk.gov.justice.laa.crime.cfecrime.cma.response.ValueList;
-
-public interface Cma {
-    ValueList callCma(CmaApiRequest request);
-}
