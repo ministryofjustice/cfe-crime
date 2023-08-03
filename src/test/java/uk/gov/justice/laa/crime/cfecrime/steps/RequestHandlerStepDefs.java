20b38525195788e00fa26be6a8ea36cc2a8bba65
Ila <ila.patel@digital.justice.gov.uk>
2023-08-03 10:30:28 +0100

    changes

--- src/test/java/uk/gov/justice/laa/crime/cfecrime/steps/RequestHandlerStepDefs.java
+++ src/test/java/uk/gov/justice/laa/crime/cfecrime/steps/RequestHandlerStepDefs.java
@@ -3,8 +3,8 @@ package uk.gov.justice.laa.crime.cfecrime.steps;
 import io.cucumber.java.ParameterType;
 import io. cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import uk.gov.justice.laa.crime.cfecrime.api.Under18;
-import uk.gov.justice.laa.crime.cfecrime.utils.RequestHandlerUtil;
+import uk.gov.justice.laa.crime.cfecrime.api.Result;
+import uk.gov.justice.laa.crime.cfecrime.utils.RequestHandler;
 
 public class RequestHandlerStepDefs {
 
@@ -13,12 +13,12 @@ public class RequestHandlerStepDefs {
         return Boolean.valueOf(value);
     }
 
-    private Under18.Outcome oc = null;
+    private Result.Outcome oc = null;
 
     @Given("Age is Under Eighteen is {string} Passport benefit is {string}")
     public void age_is_under_eighteen_is_passport_benefit_is(String under18, String passportedBenefit) {
 
-         oc = RequestHandlerUtil.getOutcomeFromAgeAndPassport(Boolean.valueOf(under18),Boolean.valueOf(passportedBenefit));
+         oc = RequestHandler.handleRequest();
     }
     @Then("the response will return {string}")
     public void the_response_will_return(String string) {
