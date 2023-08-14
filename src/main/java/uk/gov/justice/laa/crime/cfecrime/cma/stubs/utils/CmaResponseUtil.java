8d881ebfba84fdf03e3eb28caafac080d2221add
Ila <ila.patel@digital.justice.gov.uk>
2023-08-14 16:28:26 +0100

    removed unused import

--- src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/stubs/utils/CmaResponseUtil.java
+++ src/main/java/uk/gov/justice/laa/crime/cfecrime/cma/stubs/utils/CmaResponseUtil.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.crime.cfecrime.cma.stubs.utils;
 
 import uk.gov.justice.laa.crime.cfecrime.cma.stubs.LocalCmaService;
-import uk.gov.justice.laa.crime.cfecrime.interfaces.ICmaService;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.FullAssessmentResult;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.InitAssessmentResult;
 
