c2ff018c4e479b89794054d03a17c718364bbf0f
Stephen Dicks <github@starswan.com>
2023-08-17 15:50:39 +0100

    LEP-311 Part 2 - convert to spring3 compatible auth configuration (#23)

--- src/main/java/uk/gov/justice/laa/crime/cfecrime/config/SecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/crime/cfecrime/config/SecurityConfig.java
@@ -1,20 +1,21 @@
 package uk.gov.justice.laa.crime.cfecrime.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().permitAll();
-        http.csrf().disable();
+//    including oauth2 in the project attempts to secure *our* endpoints with oauth2, when what we want to do
+    // is just *talk to* an oauth2-secured endpoint (and not be secured ourselves) so we have to disable it
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable());
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll());
+        return http.build();
     }
 }
