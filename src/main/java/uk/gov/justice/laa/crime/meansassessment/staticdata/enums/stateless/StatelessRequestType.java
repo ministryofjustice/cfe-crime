7ec6d8218cb703cf1cea62994eac267de3df8555
Stephen Dicks <github@starswan.com>
2023-10-30 13:22:46 +0000

    Documentation tweaks after review

--- src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/stateless/StatelessRequestType.java
+++ src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/stateless/StatelessRequestType.java
@@ -2,7 +2,7 @@ package uk.gov.justice.laa.crime.meansassessment.staticdata.enums.stateless;
 
 import com.fasterxml.jackson.annotation.JsonClassDescription;
 
-@JsonClassDescription("Set to INITIAL for gross income test, or BOTH for gross/disposable income test (INITIAL and FULL in Crime parlance)")
+@JsonClassDescription("Levels of means test to perform. INITIAL - just the Initial Means Test. BOTH - aim to do both the Initial Means Test and Full Means Test, however doing the latter will only be performed if the Initial Means Test's result is 'FULL'")
 public enum StatelessRequestType {
     INITIAL,
     BOTH;
